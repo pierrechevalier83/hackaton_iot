f620cd44ef3572a7b4a263b8c69c49a1bcd9c754
Rui Ramos <rui@ometria.com>
2016-05-14 15:15:08 +0100

    run app

--- run_app.sh
+++ run_app.sh
@@ -9,3 +9,4 @@ git commit -a -m 'deploying'
 git push -f --set-upstream origin deploy
 ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git pull && git checkout deploy && npm run run_device"
 git branch -D deploy
+git push origin --delete deploy
