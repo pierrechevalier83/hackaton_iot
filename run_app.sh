c9c1c6b3d2bbd610611ed886331954a369e5fba7
Rui Ramos <rui@ometria.com>
2016-05-14 14:22:02 +0100

    Ignoring dist files

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo $EDISON_HOST
-ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git pull && node dist/main.js"
+ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git pull && npm run build && npm run run_device"
