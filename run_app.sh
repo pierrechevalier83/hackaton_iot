96d877d3f709bdc326424477cef64c21a5e3fc3f
Pierre Chevalier <pierrechevalier83@gmail.com>
2016-05-14 13:16:25 +0100

    run_app: stop runing when disconnect ssh

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo $EDISON_HOST
-ssh root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git pull && node main.js"
+ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git pull && node main.js"
