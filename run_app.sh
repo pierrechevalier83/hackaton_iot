62454aba1b2c31aabf33dbc44a0e1bcfcdd9a221
Rui Ramos <rui@ometria.com>
2016-05-14 15:47:23 +0100

    run app

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo $EDISON_HOST
-ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git checkout master && git pull && npm run build && npm run run_device"
+ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git checkout master && git pull && npm run build && node dist/main"
