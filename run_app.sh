dde56665680462fd988b1fa583e23e167867a3d0
Rui Ramos <rui@ometria.com>
2016-05-14 16:10:14 +0100

    Add npm install to run app

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo $EDISON_HOST
-ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git checkout master && git pull && npm run build && node dist/main"
+ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git checkout master && git pull && npm install && npm run build && node dist/main"
