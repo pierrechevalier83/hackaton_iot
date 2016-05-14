553f0eca671f0feafd79c52f446f8a3b3039b433
Rui Ramos <rui@ometria.com>
2016-05-14 17:54:38 +0100

    Removing node modules

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo $EDISON_HOST
-ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot/ && git checkout master && git pull && npm install --production && cd dist && node main"
+ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot/ && git checkout master && git pull && NODE_ENV=production npm install && cd dist && node main"
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({ port: 8990 });
+var WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({ port: 8990 });
 
 var clients = [];
+
+wss.on('open' => console.log('Listening on port 8990!'));
 
 function broadcast(data, from){
   clients
@@ -13,7 +15,7 @@ wss.on('connection', function connection(ws) {
   clients.push(ws);
 
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    console.log('received: %s', message, ws);
     broadcast(message, ws);
   });
 });
