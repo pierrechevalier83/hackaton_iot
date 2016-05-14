faef6e1460951253848cd125a42a092631d9ab16
Rui Ramos <rui@ometria.com>
2016-05-14 14:58:40 +0100

    Crazy build

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 echo $EDISON_HOST
+git checkout -B deploy
+mkdir -p dist
+npm run build
+git add -A
+git add -f dist/*
+git commit -a -m 'deploying'
+git push --set-upstream origin deploy
 ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git pull && npm run build && npm run run_device"
+git branch -D deploy
