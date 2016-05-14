53114c80f4893b0f282a63bd806abf6d2088ef70
Rui Ramos <rui@ometria.com>
2016-05-14 16:27:58 +0100

    Changing path on run app

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo $EDISON_HOST
-ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git checkout master && git pull && node dist/main"
+ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot/ && git checkout master && git pull && cd dist && node main"
