c061fa351169861cc31add42c5b2f1167ad37859
Rui Ramos <rui@ometria.com>
2016-05-14 15:02:37 +0100

    yes

--- run_app.sh
+++ run_app.sh
@@ -6,6 +6,6 @@ npm run build
 git add -A
 git add -f dist/*
 git commit -a -m 'deploying'
-git push --set-upstream origin deploy
+git push -f --set-upstream origin deploy
 ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git pull && npm run build && npm run run_device"
 git branch -D deploy
