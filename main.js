97da51acc0325b49e95a0316b1ea0a6ee597bc28
Rui Ramos <rui@ometria.com>
2016-05-14 15:26:02 +0100

    as

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ var buzzer = new buzzerModule.Buzzer(5);
 function readSensorValue() {
     if ( touch.isPressed() ) {
         console.log(touch.name() + " is pressed");
-        buzzer.playSound(buzzerModule.DO, 1000000)
+        buzzer.playSound(buzzerModule.DO, 5)
     } else {
         console.log(touch.name() + " is not pressed");
         buzzer.stopSound();
