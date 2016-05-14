4d55f2f5402329b292005aec7deb229bdc615ed1
Rui Ramos <rui@ometria.com>
2016-05-14 15:16:51 +0100

    run app

--- run_app.sh
+++ run_app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo $EDISON_HOST
-git checkout -b deploy
+git checkout -B deploy
 mkdir -p dist
 npm run build
 git add -A
@@ -8,5 +8,6 @@ git add -f dist/*
 git commit -a -m 'deploying'
 git push -f --set-upstream origin deploy
 ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git pull && git checkout deploy && npm run run_device"
+git checkout master
 git branch -D deploy
 git push origin --delete deploy
