9f8f9239a8f15e6babac7c4d83e2d9695d4b7b73
Rui Ramos <rui@ometria.com>
2016-05-14 15:34:29 +0100

    reverting

--- run_app.sh
+++ run_app.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 echo $EDISON_HOST
-git checkout -B deploy
-mkdir -p dist
-npm run build
-git add -A
-git add -f dist/*
-git commit -a -m 'deploying'
-git push --set-upstream origin deploy
-ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git reset --hard origin/deploy && npm run run_device"
-git checkout master
-git merge deploy
-git branch -D deploy
-git push origin --delete deploy
+ssh -t -t root@"$EDISON_HOST" "cd /node_app_slot/hackaton_iot && git pull && npm run build && npm run run_device"
