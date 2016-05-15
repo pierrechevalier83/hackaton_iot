e160b486cc0a8c92122901bc29fd4a56f62fc9ef
Pierre Chevalier <pierrechevalier83@gmail.com>
2016-05-15 09:45:05 +0100

    Document sensors position in comments

--- dist/sensors.js
+++ dist/sensors.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   leds: {
-    red: 6,
-    green: 7,
-    blue: 8
+    red: 6, // D6
+    green: 7, // D7
+    blue: 8 // D8
   },
-  buzzer: 5,
-  touch: 2,
-  lcd: 0,
-  rotary: 0
-};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9zZW5zb3JzLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsT0FBTyxPQUFQLEdBQWlCO0FBQ2YsUUFBTTtBQUNKLFNBQUssQ0FERDtBQUVKLFdBQU8sQ0FGSDtBQUdKLFVBQU07QUFIRixHQURTO0FBTWYsVUFBUSxDQU5PO0FBT2YsU0FBTyxDQVBRO0FBUWYsT0FBSyxDQVJVO0FBU2YsVUFBUTtBQVRPLENBQWpCIiwiZmlsZSI6InNlbnNvcnMuanMiLCJzb3VyY2VzQ29udGVudCI6WyJtb2R1bGUuZXhwb3J0cyA9IHtcbiAgbGVkczoge1xuICAgIHJlZDogNixcbiAgICBncmVlbjogNyxcbiAgICBibHVlOiA4XG4gIH0sXG4gIGJ1enplcjogNSxcbiAgdG91Y2g6IDIsXG4gIGxjZDogMCxcbiAgcm90YXJ5OiAwLFxufTtcbiJdfQ==
+  buzzer: 5, // D5
+  touch: 2, // D2
+  lcd: 0, // bottom left I2C
+  // shield needs switch to right position: 5V for text
+  rotary: 0 };
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9zZW5zb3JzLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsT0FBTyxPQUFQLEdBQWlCO0FBQ2YsUUFBTTtBQUNKLFNBQUssQ0FERCxFO0FBRUosV0FBTyxDQUZILEU7QUFHSixVQUFNLEM7QUFIRixHQURTO0FBTWYsVUFBUSxDQU5PLEU7QUFPZixTQUFPLENBUFEsRTtBQVFmLE9BQUssQ0FSVSxFOztBQVVmLFVBQVEsQ0FWTyxFQUFqQiIsImZpbGUiOiJzZW5zb3JzLmpzIiwic291cmNlc0NvbnRlbnQiOlsibW9kdWxlLmV4cG9ydHMgPSB7XG4gIGxlZHM6IHtcbiAgICByZWQ6IDYsICAgLy8gRDZcbiAgICBncmVlbjogNywgLy8gRDdcbiAgICBibHVlOiA4ICAgLy8gRDhcbiAgfSxcbiAgYnV6emVyOiA1LCAgLy8gRDVcbiAgdG91Y2g6IDIsICAgLy8gRDJcbiAgbGNkOiAwLCAgICAgLy8gYm90dG9tIGxlZnQgSTJDXG4gICAgICAgICAgICAgIC8vIHNoaWVsZCBuZWVkcyBzd2l0Y2ggdG8gcmlnaHQgcG9zaXRpb246IDVWIGZvciB0ZXh0XG4gIHJvdGFyeTogMCwgIC8vIEEwXG59O1xuIl19
--- src/sensors.js
+++ src/sensors.js
@@ -1,11 +1,12 @@
 module.exports = {
   leds: {
-    red: 6,
-    green: 7,
-    blue: 8
+    red: 6,   // D6
+    green: 7, // D7
+    blue: 8   // D8
   },
-  buzzer: 5,
-  touch: 2,
-  lcd: 0,
-  rotary: 0,
+  buzzer: 5,  // D5
+  touch: 2,   // D2
+  lcd: 0,     // bottom left I2C
+              // shield needs switch to right position: 5V for text
+  rotary: 0,  // A0
 };
